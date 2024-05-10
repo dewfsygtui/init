2ee5b85b6ae554db70598a642e569c7a43273c1f
dewfsygtui <tsjcylerstagner@hotmail.com>
2024-05-10 22:10:44 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function findMaxLength(nums) {
-  const map = new Map();
-  map.set(0, -1);
-  let count = 0;
-  let maxLength = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-      maxLength = Math.max(maxLength, i - map.get(count));
-    } else {
-      map.set(count, i);
-    }
-  }
-  return maxLength;
-}
+const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
